4fea77bc08ab0301da39edb0f5a4d331cbeae6d4
Emoless <scodaret123@gmail.com>
2024-11-26 19:47:04 +0300

    added front for POST

--- app/main.py
+++ app/main.py
@@ -2,8 +2,24 @@ import time
 from fastapi import FastAPI
 from database import get_connection
 from routers import drivers, tasks, vehicles, administrators
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",  # Разрешить доступ с локального хоста
+    "http://localhost:3000",  # Разрешить доступ с фронтенда на порту 3000 (если используется)
+    "http://127.0.0.1",  # Разрешить доступ с 127.0.0.1
+    "http://127.0.0.1:3000",  # Разрешить доступ с фронтенда на порту 3000 (если используется)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Используем список разрешённых доменов
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все методы (POST, GET, OPTIONS, и т.д.)
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
 
 # Подключение к базе данных
 while True:
